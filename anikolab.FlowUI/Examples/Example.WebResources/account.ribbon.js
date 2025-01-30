9d258eda2ed3dd442fef7794b120e3a114b8d7dd
angelonic95 <>
2025-01-30 16:23:03 +0100

    added example js

--- anikolab.FlowUI/Examples/Example.WebResources/account.ribbon.js
+++ anikolab.FlowUI/Examples/Example.WebResources/account.ribbon.js
@@ -1,1 +1,46 @@
-﻿
+﻿if (typeof (Example) === "undefined") { var Example = {}; }
+if (typeof (Example.Account) === "undefined") { Example.Account = { __namespace: true }; }
+if (typeof (Example.Account.Ribbon) === "undefined") { Example.Account.Ribbon = { __namespace: true }; }
+
+Example.Account.Ribbon.Form = new function () {
+	var _self = this;
+	let _availableActions = {};
+
+	const formTypeMapper = {
+		1: "create",
+		2: "edit",
+		3: "create"
+	};
+
+	_self.getAvailableActions = async function (formContext) {
+		let currentFormType = formContext.ui.getFormType();
+		if (!_availableActions || (_availableActions[formTypeMapper[currentFormType]] === undefined || _availableActions[formTypeMapper[currentFormType]] === null)) {
+			_availableActions[formTypeMapper[currentFormType]]  = FlowUI.Common.Flatten.getAvailableActions(formContext, "ribbon", formTypeMapper, currentFormType);
+		}
+		return _availableActions[formTypeMapper[currentFormType]];
+	};
+
+	_self.Button1Command = {
+		canExecute: async function (formContext) {
+
+			const flags = await _self.getAvailableActions(formContext);
+			return (flags?.showButton1 ?? false)
+		},
+		execute: async function (formContext) {
+			Xrm.Navigation.openAlertDialog("Click Button 1");
+		}
+	};
+
+	_self.Button2Command = {
+		canExecute: async function (formContext) {
+
+			const flags = await _self.getAvailableActions(formContext);
+			return (flags?.showButton2 ?? false)
+		},
+		execute: async function (formContext) {
+			Xrm.Navigation.openAlertDialog("Click Button 1");
+		}
+	};
+
+};
+
--- anikolab.FlowUI/Examples/Example.WebResources/incident.ribbon.js
+++ anikolab.FlowUI/Examples/Example.WebResources/incident.ribbon.js
@@ -1,1 +1,77 @@
-﻿
+﻿if (typeof (Example) === "undefined") { var Example = {}; }
+if (typeof (Example.Incident) === "undefined") { Example.Incident = { __namespace: true }; }
+if (typeof (Example.Incident.Ribbon) === "undefined") { Example.Incident.Ribbon = { __namespace: true }; }
+
+
+Example.Incident.Ribbon.Form = new function () {
+	var _self = this;
+	let _availableActions = null;
+
+	_self.getAvailableActions = async function (formContext) {
+
+		if (!_availableActions) {
+			_availableActions = FlowUI.Common.getAvailableActions(formContext, "ribbon");
+		}
+		return _availableActions;
+
+	};
+
+	_self.Button1Command = {
+		canExecute: async function (formContext) {
+
+			const flags = await _self.getAvailableActions(formContext);
+			return (flags?.showButton1 ?? false) && formContext.getAttribute("prioritycode").getValue() === 2 //Normal;
+		},
+		execute: async function (formContext) {
+			Xrm.Navigation.openAlertDialog("Click Button 1");
+		}
+	};
+
+	_self.GetInfoCommand = {
+		canExecute: async function (formContext) {
+
+			const flags = await _self.getAvailableActions(formContext);
+			return flags?.showGetInfoFlyOut ?? false
+		},
+		execute: async function (formContext) {
+			Xrm.Navigation.openAlertDialog("Click Button 3");
+		}
+	};
+
+	_self.GetIdInfoCommand = {
+		canExecute: async function (formContext) {
+
+			const flags = await _self.getAvailableActions(formContext);
+			return flags?.showGetId ?? false
+		},
+		execute: async function (formContext) {
+			let recordId = formContext.data.entity.getId();
+			Xrm.Navigation.openAlertDialog("ID : " + recordId);
+		}
+	};
+
+	_self.GetEntityInfoCommand = {
+		canExecute: async function (formContext) {
+
+			const flags = await _self.getAvailableActions(formContext);
+			return flags?.showGetEntityName ?? false
+		},
+		execute: async function (formContext) {
+			let entityName = formContext.data.entity.getEntityName();
+			Xrm.Navigation.openAlertDialog("Entity Name: " + entityName);
+		}
+	};
+
+	_self.GetNameCommand = {
+		canExecute: async function (formContext) {
+
+			const flags = await _self.getAvailableActions(formContext);
+			return flags?.showGetName ?? false
+		},
+		execute: async function (formContext) {
+			let title = formContext.getAttribute("title").getValue();
+			Xrm.Navigation.openAlertDialog("Title: " + title);
+		}
+	}
+};
+
